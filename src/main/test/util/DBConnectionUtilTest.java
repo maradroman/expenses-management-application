c92dc93756bdc3a16e2dbd77e97060b1812de51f
Roma M <maradroman@gmail.com>
2021-03-25 15:07:37 +0200

    database connection test added

--- src/main/test/util/DBConnectionUtilTest.java
+++ src/main/test/util/DBConnectionUtilTest.java
@@ -2,12 +2,19 @@ package util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DBConnectionUtilTest {
 
     @Test
     void openConnection() {
         assertDoesNotThrow(() -> DBConnectionUtil.openConnection());
+    }
+
+    @Test
+    void checkIfOpenConnectionReturnsNotNullValue(){
+        assertNotNull(DBConnectionUtil.openConnection());
     }
 }
